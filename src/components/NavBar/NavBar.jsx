d5a9a376a6fbc35698bc7832baed37894a013e89
malo-dev <leadermushio377@gmail.com>
2023-02-09 15:28:53 +0200

    fix:feedback

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Gda from '../../assets/GDA_Project.png'
+import Gda from '../../assets/GDA_Project.svg'
 
 const NavBar = () => {
 	return (
-		<div className=' container flex flex-row  justify-between  items-center  bg-[#FFFFFF;]  '>
-			<img src={Gda} alt="" className='w-[155.22px] h-[23.66px]   h-[23.66px]' />
-			<div className='container w-[357px] h-[24px] font-[IBM Plex Sans] text-[] flex justify-center items-center flex-row gap-[31px]'>
-				<h3 className='font-[600] font-[IBM Plex Sans] text-[#1F2325] opacity-[80%]'>Projects</h3>
-				<h3 className='font-[600]'>Blogs</h3>
-				<h3 className='font-[600]'>{ 'A propos'}</h3>
+		<div className=' container flex flex-row  justify-between  items-center  bg-white  '>
+			<img src={Gda} alt="" className='w-[155.22px] h-[23.66px]' />
+			<div  className='hidden sm:flex lg:flex xl:flex container w-1/4  md:w-1/5  h-24 font-[IBM Plex Sans] opacity-80 text-[16px] text-[#1F2325]  font-normal flex justify-center items-center flex-row gap-[31px]'>
+				<h3 className='font-bold '>Projects</h3>
+				<h3 className='font-bold'>Blogs</h3>
+				<h3 className='font-bold'>{ 'A propos'}</h3>
 			</div>
 		</div>
 	)
