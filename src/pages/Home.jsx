0f38480ba69667d77701b399646da13d03023fad
malo-dev <leadermushio377@gmail.com>
2023-02-13 08:50:21 +0200

    feat:technologie article

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import CardArticle from '../components/cardArticle/cardArticle';
 import Hero from "../components/hero/Hero";
 import NavBar from "../components/NavBar/NavBar";
 import ProjetSection from "../components/projet/ProjetSection";
+import TechProject from '../components/techProject/TechProject';
 
 export default function Home() {
   return (
     <div className="flex flex-col ">
       <NavBar />
       <Hero />
+      <TechProject/>
       <ProjetSection />
       <CardArticle/>
     </div>
