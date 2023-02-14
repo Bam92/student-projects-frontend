c47ae15768d7df8415baf11464b2ba2b2d52e33c
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2023-02-14 11:54:29 +0200

    implemente sectionStudent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import Footer from "../components/footer/Footer";
 import Hero from "../components/hero/Hero";
 import NavBar from "../components/NavBar/NavBar";
 import ProjetSection from "../components/projet/ProjetSection";
+import Student from "../components/student/Student";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-12">
+    <div className="flex flex-col gap-12 ">
       <NavBar />
       <Hero />
+      <Student />
       <ProjetSection />
       <Footer />
     </div>
