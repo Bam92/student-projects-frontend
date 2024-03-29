e1398115f6c4911672ce6d82632681313fd517a9
Cedric Karungu <ckarungu921@gmail.com>
2024-03-29 20:35:52 +0200

    fix: header styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#D92950',
+			},
+		},
+	},
+	plugins: [],
 };
