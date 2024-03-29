2b6177fb739385dfe92ea2bec083c46828bd656d
Cedric Karungu <ckarungu921@gmail.com>
2024-03-29 21:50:50 +0200

    fix: tech skilss

---
+++ src/components/techProject/BarProjet.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { useState } from 'react';
+function BarProjet() {
+	const tech = ['Compentences', 'Outils'];
+	const [isActive, setActive] = useState({
+		keyAct: 0,
+	});
+	return (
+		<section className='flex py-2 my-4'>
+			<div className='flex  p-1 rounded-full shadow-lg items-center bg-[#303030]'>
+				{tech &&
+					tech.map((techno, key) => (
+						<span
+							onClick={() => {
+								setActive({ keyAct: key });
+							}}
+							className={
+								isActive.keyAct === key
+									? 'bg-[#D92950] text-white p-2 px-3 rounded-full'
+									: 'text-white hover:bg-[#d9294f9a] p-1 px-3 rounded-full'
+							}
+							key={key}
+						>
+							{techno}
+						</span>
+					))}
+			</div>
+		</section>
+	);
+}
+
+export default BarProjet;
--- src/components/techProject/TechProject.jsx
+++ src/components/techProject/TechProject.jsx
@@ -1,120 +1,101 @@
-import React from 'react'
-import js from '../../../public/javascript.svg.svg'
-import sass from '../../../public/sass.svg.svg'
-import b from '../../../public/b.png'
-import react from '../../../public/react.png'
-import f from '../../../public/f.png'
+import React from 'react';
+import js from '../../../public/javascript.svg.svg';
+import sass from '../../../public/sass.svg.svg';
+import b from '../../../public/b.png';
+import react from '../../../public/react.png';
+import f from '../../../public/f.png';
 
-import n from '../../../public/n.png'
-import node from '../../../public/node.png'
-import red from '../../../public/red.png'
-import first from '../../../public/first.png'
+import n from '../../../public/n.png';
+import node from '../../../public/node.png';
+import red from '../../../public/red.png';
+import first from '../../../public/first.png';
 
-import redis from '../../../public/redis.png'
-import ts from '../../../public/ts.png'
-import v from '../../../public/v.png'
-import post from '../../../public/post.png'
-import mongo from   '../../../public/mongo.png'
+import redis from '../../../public/redis.png';
+import ts from '../../../public/ts.png';
+import v from '../../../public/v.png';
+import post from '../../../public/post.png';
+import mongo from '../../../public/mongo.png';
+import BarProjet from './BarProjet';
 const TechProject = () => {
 	return (
-		<div className='flex pt-[240px]  md:flex-column lg:flex-row xd:flex-row  '>
-			<div>
-				<section id='center' className='text-left w-[100%] max-[680px]:w-[100%]'>
-				<h1 className='text-[28px] max-[680px]:text-[32px] w-[70%] font-bold'>
-					Les technologies  utiliser dans leurs differents projets
-				</h1>
-					<p className=' text-[18px] text-[#263238]'>
-						Nous utiisons differentes technologies pour créer
-						la plus part<br/>
-						de nos projets, on est polyvalents.
-						vous trouverai une panoplis <br/>des
-						technos utiliser qui vont cadrer avec vos attentes
-				
-				</p>
-			</section>
-			</div>
-			<main className='flex flex-col gap-5'>
-				<section className='flex flex-row  gap-7'>
-					<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={sass} alt="" className='bg-center ' />
-					</div>
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={js} alt="" className='bg-center ' />
-					</div>
-					
-					
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={ts} alt="" className='bg-center ' />
-					</div>
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={first} alt="" className='bg-center ' />
-					</div>
-					<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={b} alt="" className='bg-center ' />
-					</div>
-				</section>
-				
-				
-				
-				
-				
-				
-				<section className='flex flex-row  gap-7'>
-					<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={react} alt="" className='bg-center ' />
-					</div>
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={n} alt="" className='bg-center ' />
-					</div>
-					
-					
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={v} alt="" className='bg-center ' />
-					</div>
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={f} alt="" className='bg-center ' />
-					</div>
-					<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={node} alt="" className='bg-center ' />
-					</div>
-				</section>
-				
-				
-				
-				
-				
-				
-				
-				
-					<section className='flex flex-row  gap-7 px-[30px]'>
-					<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={red} alt="" className='bg-center ' />
-					</div>
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={redis} alt="" className='bg-center ' />
-					</div>
-					
-					
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={post} alt="" className='bg-center ' />
-					</div>
-						<div className='bg-white drop-shadow-md text-center flex justify-center items-center w-[70px] h-[70px] '>
-							<img src={mongo} alt="" className='bg-center ' />
-					</div>
-					
-				</section>
-				
-				
-				
-					<section>
-						
+		<div className='mx-auto container'>
+			<div className='flex justify-between'>
+				<div>
+					<section className='text-left max-[680px]:w-[100%] flex flex-col '>
+						<h1 className='text-5xl leading-[1.4] my-4 w-[70%] font-bold'>
+							Les technologies utiliser dans leurs differents projets
+						</h1>
+						<p className=' text-lg text-gray-600'>
+							Nous utiisons differentes technologies pour créer la plus part
+							<br />
+							de nos projets, on est polyvalents. vous trouverai une panoplis{' '}
+							<br />
+							des technos utiliser qui vont cadrer avec vos attentes
+						</p>
+						<BarProjet />
 					</section>
-					<section>
-						
-					</section>
-				</main>
-		</div>
-	)
-}
+				</div>
+				<main className='flex flex-col gap-5'>
+					<section className='flex flex-row  gap-7'>
+						<div className='bg-white flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={sass} alt='' className='' />
+						</div>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={js} alt='' className='' />
+						</div>
 
-export default TechProject
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={ts} alt='' className='' />
+						</div>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={first} alt='' className='' />
+						</div>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={b} alt='' className='' />
+						</div>
+					</section>
+
+					<section className='flex flex-row  justify-center gap-7'>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={react} alt='' className='' />
+						</div>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={n} alt='' className='' />
+						</div>
+
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={v} alt='' className='' />
+						</div>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={f} alt='' className='' />
+						</div>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={node} alt='' className='' />
+						</div>
+					</section>
+
+					<section className='flex flex-row  justify-center gap-7 px-[30px]'>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={red} alt='' className='' />
+						</div>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={redis} alt='' className='' />
+						</div>
+
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={post} alt='' className='' />
+						</div>
+						<div className='bg-white  flex justify-center items-center w-28 h-28 rounded-xl drop-shadow-2xl '>
+							<img src={mongo} alt='' className='' />
+						</div>
+					</section>
+
+					<section></section>
+					<section></section>
+				</main>
+			</div>
+		</div>
+	);
+};
+
+export default TechProject;
